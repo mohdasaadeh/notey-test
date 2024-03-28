780c8de4d5e6ba3b31539a79b2a43467c088ebfe
mohdasaadeh <m.saadeh@wajeez.co>
2024-03-28 15:02:26 +0300

    adding more test cases

--- test-model.mjs
+++ test-model.mjs
@@ -15,7 +15,62 @@ describe("Initialize", function () {
       store = await useNotesModel(process.env.NOTES_MODEL);
     } catch (e) {
       console.error(e);
+
       throw e;
     }
+  });
+});
+
+describe("Model Test", function () {
+  describe("check keylist", function () {
+    before(async function () {
+      await store.create("n1", "Note 1", "Note 1");
+      await store.create("n2", "Note 2", "Note 2");
+      await store.create("n3", "Note 3", "Note 3");
+    });
+
+    it("should have three entries", async function () {
+      const keyz = await store.keylist();
+
+      assert.exists(keyz);
+      assert.isArray(keyz);
+      assert.lengthOf(keyz, 3);
+    });
+
+    it("should have keys n1 n2 n3", async function () {
+      const keyz = await store.keylist();
+
+      assert.exists(keyz);
+      assert.isArray(keyz);
+      assert.lengthOf(keyz, 3);
+
+      for (let key of keyz) {
+        assert.match(key, /n[123]/, "correct key");
+      }
+    });
+
+    it("should have titles Note #", async function () {
+      const keyz = await store.keylist();
+
+      assert.exists(keyz);
+      assert.isArray(keyz);
+      assert.lengthOf(keyz, 3);
+
+      const keyPromises = keyz.map((key) => store.read(key));
+
+      const notez = await Promise.all(keyPromises);
+
+      for (let note of notez) {
+        assert.match(note.title, /Note [123]/, "correct title");
+      }
+    });
+
+    after(async function () {
+      const keyz = await store.keylist();
+
+      for (let key of keyz) {
+        await store.destroy(key);
+      }
+    });
   });
 });
